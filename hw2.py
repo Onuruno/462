47bbab383f6cf61c0e8156de5f1889d65c97975b
Onur Gökce <76440688+Onuruno@users.noreply.github.com>
2021-11-22 19:48:11 +0300

    Create hw2.py

---
+++ hw2.py
@@ -0,0 +1,191 @@
+def copy2(a):
+    b=[]
+    for i in range(len(a)):
+        b.append(a[i].copy())
+    return b
+
+def copy3(a):
+    b=[]
+    for i in range(len(a)):
+        c=copy2(a[i])
+        b.append(c.copy())
+    return b
+
+def isPuzzleCompleted(puzzleArray):
+    if (puzzleArray == [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '0']]):
+        return True
+    else:
+        return False
+    
+def isMoveLegal(puzzleArray, move):
+    if(move == 'L'):
+        for item in puzzleArray:
+            if(item[0] == '0'):
+                return False
+        return True
+    elif(move == 'U'):
+        for item in puzzleArray[0]:
+            if(item == '0'):
+                return False
+        return True
+    elif(move == 'R'):
+        for item in puzzleArray:
+            if(item[2] == '0'):
+                return False
+        return True
+    elif(move == 'D'):
+        for item in puzzleArray[2]:
+            if(item == '0'):
+                return False
+        return True
+
+def makeMove(puzzleArray, move):
+    for i in range(3):
+        for j in range(3):
+            if(puzzleArray[i][j] == '0'):
+                row=i
+                column=j            
+    if(move == 'L'):
+        puzzleArray[row][column] = puzzleArray[row][column-1]
+        puzzleArray[row][column-1] = '0'
+        return puzzleArray
+    elif(move == 'U'):
+        puzzleArray[row][column] = puzzleArray[row-1][column]
+        puzzleArray[row-1][column] = '0'
+        return puzzleArray
+    elif(move == 'R'):
+        puzzleArray[row][column] = puzzleArray[row][column+1]
+        puzzleArray[row][column+1] = '0'
+        return puzzleArray
+    elif(move == 'D'):
+        puzzleArray[row][column] = puzzleArray[row+1][column]
+        puzzleArray[row+1][column] = '0'
+        return puzzleArray
+
+def fixPath(path):
+    result=[]
+    for item in path:
+        new_str=''
+        for i in item:
+            for j in i:
+                new_str+=j
+        a= list(new_str)
+        a[a.index('0')]=' '
+        new_str=''.join(a)
+        result.append(new_str)
+    return result
+
+def heuristicPuzzle(puzzleArray):
+    cost=0
+    for i in range(3):
+        for j in range(3):
+            if(puzzleArray[i][j] == '0'):
+                pass
+                
+def ucsMaze(start, end, mazeArray):
+    return mazeArray
+
+def aStarMaze(star, end, mazeArray):
+    return mazeArray
+        
+def ucsPuzzle(puzzleArray):
+    queue = []
+    queue.append(([puzzleArray], []))
+    visited = []
+    while(len(queue)>0):
+        path = queue[0][0]
+        moves = queue[0][1]
+        selectedPuzzle = path[-1]
+        queue.remove(queue[0])
+        if(selectedPuzzle in visited):
+            continue
+        if(isPuzzleCompleted(selectedPuzzle)):
+            return moves, fixPath(path), len(moves), len(moves)
+        else:
+            if(isMoveLegal(selectedPuzzle, 'L')):
+                newPuzzle = copy2(selectedPuzzle)
+                newPath = copy3(path)
+                newMoves = moves.copy()
+                
+                makeMove(newPuzzle, 'L')
+                
+                newPath.append(newPuzzle)
+                newMoves.append('LEFT')
+                queue.append((newPath, newMoves))
+                
+            if(isMoveLegal(selectedPuzzle, 'U')):
+                newPuzzle = copy2(selectedPuzzle)
+                newPath = copy3(path)
+                newMoves = moves.copy()
+                
+                makeMove(newPuzzle, 'U')
+                
+                newPath.append(newPuzzle)
+                newMoves.append('UP')
+                queue.append((newPath, newMoves))
+                
+            if(isMoveLegal(selectedPuzzle, 'R')):
+                newPuzzle = copy2(selectedPuzzle)
+                newPath = copy3(path)
+                newMoves = moves.copy()
+                
+                makeMove(newPuzzle, 'R')
+                
+                newPath.append(newPuzzle)
+                newMoves.append('RIGHT')
+                queue.append((newPath, newMoves))
+                
+            if(isMoveLegal(selectedPuzzle, 'D')):
+                newPuzzle = copy2(selectedPuzzle)
+                newPath = copy3(path)
+                newMoves = moves.copy()
+                
+                makeMove(newPuzzle, 'D')
+                
+                newPath.append(newPuzzle)
+                newMoves.append('DOWN')
+                queue.append((newPath, newMoves))
+        visited.append(copy2(selectedPuzzle))    
+    return None
+
+def aStarPuzzle(puzzleArray):
+    return puzzleArray
+
+def InformedSearch(method_name, problem_file_name):
+    with open(problem_file_name) as file:
+        lines = file.read().splitlines()
+        
+    if(problem_file_name[0] == 'm'):
+        start = lines[0]
+        end = lines[1]
+        mazeArray = []
+        for item in lines[2:]:
+            mazeline = []
+            for character in item:
+                if(character == " "):
+                    mazeline.append(0)
+                elif(character == "#"):
+                    mazeline.append(1)
+            mazeArray.append(mazeline)
+        
+        if(method_name == "UCS"):
+            return ucsMaze(start, end, mazeArray)
+        elif(method_name == "AStar"):
+            return aStarMaze(start, end, mazeArray)
+                
+    elif(problem_file_name[0] == 'e'):
+        puzzleArray = []
+        for item in lines[:3]:
+            puzzleArray.append(item.split())
+     
+        if(method_name == "UCS"):
+            return ucsPuzzle(puzzleArray)
+        elif(method_name == "AStar"):
+            return aStarPuzzle(puzzleArray)  
+
+
+#print(InformedSearch("UCS", "eightpuzzle1.txt"))
+a = [['6', '1', '2'], ['4', '0', '3'], ['7', '8', '5']]
+b=[]
+
+#print(ucsPuzzle(a))
